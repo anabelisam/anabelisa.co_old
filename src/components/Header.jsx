cc29f00ab4ffdf9b58379e56f133b85878361e8a
Ana Belisa <ana.belisa9403>
2019-05-22 23:20:41 -0500

    Change name of profilepic (temporarily) in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'gatsby';
 
-const profilePic = 'https://s3.amazonaws.com/drgns/profile-pic.png';
+const profilePic = 'https://s3.amazonaws.com/chewiekie/img/anabelisa.png';
 
 const Header = ({ title }) => (
   <div className="Header">
