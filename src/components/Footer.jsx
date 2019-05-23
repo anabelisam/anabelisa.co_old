e272bebccf560b8ee5652bff02a41cbb67d8ea3a
Ana Belisa <ana.belisa9403>
2019-05-22 23:19:50 -0500

    Add line of text in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => (
   <footer>
-    <span>2019. Bogotá - Colombia</span>
+    <span>2019. Bogotá - Colombia - Pa' que te lo goce'</span>
     <span><a href="/rss.xml">Rss</a></span>
   </footer>
 );
